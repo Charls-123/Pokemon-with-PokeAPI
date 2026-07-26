628e2757d6a421b0944427b72747eed0abed1d3b
Charly <Cgrijalvacastillo@gmail.com>
2026-07-25 21:35:34 -0700

    implementacion de limpiador para boton de busqueda

--- JS/app.js
+++ JS/app.js
@@ -5,13 +5,16 @@ const api = new DatosPokemon();
 const ui = new uiPagina();
 
 function cargarPagina() {
-    const btnBusqueda = document.querySelector('#btnBuscar')
+    const btnBusqueda = document.querySelector('#btnBuscar');
+    const contenedorPokemones = document.querySelector('#pokemones')
 
     btnBusqueda.addEventListener('click', async () => {
         const nombre = document.querySelector('#busqueda').value;
         const url = await api.pokemon(nombre)
 
         if (url) {
+            ui.limpiarContenedor(contenedorPokemones);
+
             ui.imprimirPokemon(url);
         }
         
--- JS/ui.js
+++ JS/ui.js
@@ -1,6 +1,39 @@
 export class uiPagina {
-    async imprimirPokemon(url) {
-        const {abilities, name, types, sprites, id} = await url;
+    obtenerImagenesTipos(tipoNombre) {
+        const  url = `https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/${tipoNombre}.svg`;
+
+        return `
+            <img
+                src = ${url}
+                alt = '${tipoNombre}'
+                class = 'tipo'
+            >                
+        `;
+    }
+
+    obtenerClaseTipos(tipo) {
+        const tipos = {
+            'fuego': 'bg-fuego',
+            'agua': 'bg-agua',
+            'planta': 'bg-planta',
+            'veneno': 'bg-veneno',
+            'normal': 'bg-normal',
+            'volador': 'bg-volador',
+            'tierra': 'bg-tierra',
+            'roca': 'bg-roca',
+            'hielo': 'bg-hielo',
+            'acero': 'bg-acero',
+            'dragon': 'bg-dragon',
+            'hada': 'bg-hada',
+            'siniestro': 'bg-siniestro',
+            'psiquico': 'bg-psiquico',
+            'electrico': 'bg-electrico',
+            'lucha': 'bg-lucha',
+        }
+    }
+
+    imprimirPokemon(url) {
+        const {abilities, name, types, sprites, id} = url;
         
         const contenedorCards = document.querySelector('#pokemones')
         const urlImagen = url.sprites.other['official-artwork'].front_default;
@@ -21,26 +54,33 @@ export class uiPagina {
         const imgPoke = document.createElement('img');
         imgPoke.src = urlImagen;
 
+
         const contenedorTipo = document.createElement('div');
+        contenedorTipo.classList.add('tipos')
 
         contenedorTipo.innerHTML = `
-        ${
-            abilities.forEach(habilidad => {
-                
-            })
-        }`
-
-        
-
+            ${
+                types.map( tipo => {
+                    return this.obtenerImagenesTipos(tipo.type.name)
+                    }).join('')
+            }
+        `
 
         noNombre.appendChild(noPokedex)
         noNombre.appendChild(nombre)
 
         card.appendChild(noNombre);
         card.appendChild(imgPoke);
+        card.appendChild(contenedorTipo);
 
 
         contenedorCards.appendChild(card)
         
+    }
+
+    limpiarContenedor(contenedor) {
+        while(contenedor.firstChild) {
+            contenedor.removeChild(contenedor.firstChild)
+        }
     }
 }
